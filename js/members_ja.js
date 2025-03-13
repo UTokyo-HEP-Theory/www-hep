ce89103d4f0e7d958ab213d73bc2cce13f693ccf
Kantaro Ohmori <kant.ohmori@gmail.com>
2025-03-13 16:13:31 +0900

    type casing  leave.year and leave.month to avoid comparing strings.

--- js/members_ja.js
+++ js/members_ja.js
@@ -62,8 +62,10 @@ function shouldDisplay(member) {
 
     // if the leave date is in the past return false
     if (member.period.leave !== null && member.period.leave !== undefined) {
-        if (member.period.leave.year < currentYear ||
-            (member.period.leave.year === currentYear && member.period.leave.month < currentMonth)) {
+        const leaveYear = parseInt(member.period.leave.year, 10);
+        const leaveMonth = parseInt(member.period.leave.month, 10);
+        if (leaveYear < currentYear ||
+            (leaveYear === currentYear && leaveMonth < currentMonth)) {
             return false;
         }
     }
